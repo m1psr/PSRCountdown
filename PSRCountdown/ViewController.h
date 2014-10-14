2bc67596bcb4f5b18f6eb6f78ce0fccc621e5ada
Mikhai1 <m@MacBook-Air-M.local>
2014-10-14 16:59:04 +0400

    Initial Commit

---
+++ PSRCountdown/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PSRCountdown
+//
+//  Created by M on 14.10.14.
+//  Copyright (c) 2014 M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PSRCountdown/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PSRCountdown
+//
+//  Created by M on 14.10.14.
+//  Copyright (c) 2014 M. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
